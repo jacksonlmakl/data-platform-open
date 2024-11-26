de87660bb8048a7f37182129d48d3c69be811820
Jackson Makl <jackson.makl@dataiku.com>
2024-11-26 16:22:15 -0500

    dagster

--- orchestration/my_project/my_project/jobs/replication_job.py
+++
@@ -1,6 +0,0 @@
-from dagster import job
-from my_project.ops.replication_op import run_replication_script
-
-@job
-def replication_job():
-    run_replication_script()
--- orchestration/my_project/my_project/schedule.py
+++
@@ -1,6 +0,0 @@
-from dagster import schedule
-from my_project.jobs.replication_job import replication_job
-
-@schedule(cron_schedule="0 9 * * *", job=replication_job, execution_timezone="UTC")
-def daily_replication_schedule():
-    return {}
